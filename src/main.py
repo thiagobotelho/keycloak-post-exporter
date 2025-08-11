fc5b329a8bcc9d5193e4e2dc90c0170ec46341ff
Thiago de Oliveira Botelho <38995115+thiagobotelho@users.noreply.github.com>
2025-08-11 15:03:39 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -23,10 +23,10 @@ if not KEYCLOAK_URL or not CLIENT_ID or not CLIENT_SECRET:
     raise ValueError("As variáveis de ambiente KEYCLOAK_URL, CLIENT_ID e CLIENT_SECRET são obrigatórias.")
 
 # Logs iniciais
-print("🔄 Inicializando Keycloak POST Exporter na porta 8000...", flush=True)
-print(f"🌐 KEYCLOAK_URL: {KEYCLOAK_URL}", flush=True)
-print(f"🔐 CLIENT_ID: {CLIENT_ID}", flush=True)
-print(f"📛 INSTANCE: {INSTANCE}, NAMESPACE: {NAMESPACE}, POD: {POD}", flush=True)
+print("Inicializando Keycloak POST Exporter na porta 8000...", flush=True)
+print(f"KEYCLOAK_URL: {KEYCLOAK_URL}", flush=True)
+print(f"CLIENT_ID: {CLIENT_ID}", flush=True)
+print(f"INSTANCE: {INSTANCE}, NAMESPACE: {NAMESPACE}, POD: {POD}", flush=True)
 
 # Métrica com labels
 keycloak_post_duration = Gauge(
@@ -39,7 +39,7 @@ keycloak_post_duration = Gauge(
 def measure_loop():
     while True:
         try:
-            print("➡️ Iniciando requisição POST ao Keycloak...", flush=True)
+            print("➡Iniciando requisição POST ao Keycloak...", flush=True)
             start = time.time()
             response = requests.post(
                 KEYCLOAK_URL,
@@ -53,10 +53,9 @@ def measure_loop():
             )
             duration = time.time() - start
             keycloak_post_duration.labels(INSTANCE, NAMESPACE, POD).set(duration)
-            print(f"✅ POST bem-sucedido em {duration:.3f}s (status {response.status_code})", flush=True)
 
         except Exception as e:
-            print(f"❌ Erro ao fazer POST: {e}", flush=True)
+            print(f"Erro ao fazer POST: {e}", flush=True)
             keycloak_post_duration.labels(INSTANCE, NAMESPACE, POD).set(-1)
 
         time.sleep(60)
